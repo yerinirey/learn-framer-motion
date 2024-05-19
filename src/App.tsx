eb0a3f269560fb0334c879aeaf93cfa559a89f10
yerinirey <yerin886@naver.com>
2024-05-19 16:16:13 +0900

    8.2 Basic Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,18 +9,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 20%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: "spring", bounce: 0.4, delay: 0.3 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 90 }}
+      />
+      <span>.</span>
     </Wrapper>
   );
 }
