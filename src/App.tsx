5206bbf69098d2f21f0857c9acda97c99f331069
yerinirey <yerin886@naver.com>
2024-05-19 16:42:07 +0900

    8.4 Variants part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, spring } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,26 +10,75 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 20%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+const Circle = styled(motion.div)`
+  background-color: white;
+  place-self: center;
+  height: 70px;
+  width: 70px;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
-const AnimationVars = {
-  start: { scale: 0 },
+// const Box = styled(motion.div)`
+//   width: 200px;
+//   height: 200px;
+//   background-color: white;
+//   border-radius: 20%;
+//   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+// `;
+// const AnimationVars = {
+//   start: { scale: 0 },
+//   end: {
+//     scale: 1,
+//     rotateZ: 90,
+//     transition: { type: "spring", bounce: 0.4, delay: 0.3 },
+//   },
+// };
+const boxVars = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 90,
-    transition: { type: "spring", bounce: 0.4, delay: 0.3 },
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.2,
+      staggerChildren: 0.1,
+    },
   },
 };
-
+const circleVars = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 function App() {
   return (
     <Wrapper>
-      <Box variants={AnimationVars} initial="start" animate="end" />
+      {/* <Box variants={AnimationVars} initial="start" animate="end" /> */}
+      <Box variants={boxVars} initial="start" animate="end">
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+      </Box>
       <span>.</span>
     </Wrapper>
   );
